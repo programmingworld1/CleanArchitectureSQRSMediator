9c15ba1b09b8b1119b50ee761fe2d783be02b22f
= <=>
2026-02-04 11:35:59 +0100

    fix

--- Infrastructure.Persistance/Persistance/Configurations/ArtistConfiguration.cs
+++ Infrastructure.Persistance/Persistance/Configurations/ArtistConfiguration.cs
@@ -16,8 +16,12 @@ namespace Infrastructure.Persistance.Configurations
                 .IsUnique();
 
             builder.Property(a => a.RowVersion)
-                .IsRowVersion()
-                .IsConcurrencyToken();
+                .IsRowVersion() // For each update/insert it automatically creates a new token in the db in the column
+
+                // used for : Last Write Wins (lost update / / lost race / write-write conflict), Inconsistent State Prevention
+                // EF core uses the field for optimisci concurreny control, meaning an exception will be thrown when there is concurrency (with pessimistic the second user cant even retrieve the record because of a hard lock)
+                // the issue with this, it's either all or nothing, if inconsisten state prevention is not an issue but last write wins is.. in that case IsConcurrencyToken can be used for each property you want the check to happen
+                .IsConcurrencyToken(); 
         }
     }
 }
